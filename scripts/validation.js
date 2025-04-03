6c5ea50c6988fc0acaa3192568ae6645d5a9a9f4
Arturo Cerda <thisisrealartt@gmail.com>
2025-04-03 13:11:07 -0400

    Validation Script

---
+++ scripts/validation.js
@@ -0,0 +1,60 @@
+function enableValidation(options) {
+  const formEls = (...document.querySelectorAll(".modal__form"));
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit",(e) => {
+      e.preventDefault();
+  });
+  // look for all inputs inside of form 
+  // loop through all the inputs to see if all are valid
+    //if input is not valid
+    // get validation message
+      // add error class to input
+      // display error message
+      //disable button
+    // if all inputs are valid
+      // enable button
+      //reset error messages
+}};
+}
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+enableValidation(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
